561a728c34ecedce31e8582c527a1d356cc6abf9
Brice Aka <briceclaross@gmail.com>
2025-03-13 14:25:57 +0100

    page d'edition crée

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,14 @@ import { provideRouter, Routes } from '@angular/router';
 import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
 import { PokemonProfileComponent } from './pokemon/pokemon-profile/pokemon-profile.component';
 import { PageNoFoundComponent } from './page-no-found/page-no-found.component';
+import { PokemonEditComponent } from './pokemon/pokemon-edit/pokemon-edit.component';
 
 
 const routes: Routes = [
+  { path: 'pokemons/edit/:id',
+    component: PokemonEditComponent,
+    title: "Edition d'un pokemon"  },
+
   { path: 'pokemons/:id', component: PokemonProfileComponent, title: 'Pokemon'  },
   { path: 'pokemons', component: PokemonListComponent , title: 'Pokedex'},
   { path: '', redirectTo: '/pokemons', pathMatch:'full'},
