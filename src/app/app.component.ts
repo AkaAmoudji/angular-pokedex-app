0ad69d640181a58ad6c00ec4adf57709b440f545
Brice Aka <briceclaross@gmail.com>
2025-03-13 13:46:27 +0100

    Ajout de la page 404 et mise à jour de l'application Pokémon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
-import { Pokemon } from './pokemon.model';
-import { PokemonBorderDirective } from './pokemon-border.directive';
-import { DatePipe } from '@angular/common';
-import { PokemonService } from './pokemon.service';
+
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [PokemonBorderDirective, DatePipe, ],
+  imports: [RouterOutlet, RouterLink,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -18,40 +16,5 @@ export class AppComponent {
   
   
   
-  readonly #pokemonService = inject(PokemonService);
-  readonly pokemonList = signal(this.#pokemonService.getPokemonList());
-  
-  readonly searchTerm = signal('')
-
-  readonly pokemonListFiltered = computed(() => {
-    const searchTerm = this.searchTerm();
-    const pokemonList = this.pokemonList();
-
-    return pokemonList.filter(pokemon => 
-      pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-
-  })
-  
-  size(pokemon : Pokemon) {
-    if(pokemon.life < 15) {
-      return 'petit'
-    }
-
-    if(pokemon.life > 25) {
-      return'grand'
-    }
-    return 'moyen'
-
-  }
-
- 
-  incrementeLife(pokemon : Pokemon) {
-    pokemon.life = pokemon.life + 1;
-  }
-  decrementeLife(pokemon : Pokemon) {
-    pokemon.life = pokemon.life - 1;
-    
-  }
 
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, Routes } from '@angular/router';
+import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
+import { PokemonProfileComponent } from './pokemon/pokemon-profile/pokemon-profile.component';
+import { PageNoFoundComponent } from './page-no-found/page-no-found.component';
 
+
+const routes: Routes = [
+  { path: 'pokemons/:id', component: PokemonProfileComponent, title: 'Pokemon'  },
+  { path: 'pokemons', component: PokemonListComponent , title: 'Pokedex'},
+  { path: '', redirectTo: '/pokemons', pathMatch:'full'},
+  { path: '**', component: PageNoFoundComponent },
+];
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true })]
+  
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes)
+
+  ]
 };
