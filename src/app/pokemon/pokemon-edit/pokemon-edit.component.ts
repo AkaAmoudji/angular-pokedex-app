facad02b9a7275f27ce00b9eac5f2e2c912ebaad
AkaAmoudji <briceclaross@gmail.com>
2025-07-02 13:20:57 +0200

    Ajout des règles de validations pour le champ name

--- src/app/pokemon/pokemon-edit/pokemon-edit.component.ts
+++ src/app/pokemon/pokemon-edit/pokemon-edit.component.ts
@@ -2,8 +2,14 @@ import { DatePipe } from '@angular/common';
 import { Component, inject, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { PokemonService } from '../../pokemon.service';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { getPokemonColor } from '../../pokemon.model';
+import {
+   FormArray,
+   FormControl,
+   FormGroup,
+   ReactiveFormsModule,
+   Validators
+}from '@angular/forms';
+import { getPokemonColor, POKEMON_RULES } from '../../pokemon.model';
 
  
  @Component({
@@ -24,7 +30,12 @@ import { getPokemonColor } from '../../pokemon.model';
    ).asReadonly();
 
    readonly form = new FormGroup({
-    name: new FormControl(this.pokemon().name),
+    name: new FormControl(this.pokemon().name, [
+      Validators.required,
+      Validators.minLength(POKEMON_RULES.MIN_NAME),
+      Validators.maxLength(POKEMON_RULES.MAX_NAME),
+      Validators.pattern(POKEMON_RULES.NAME_PATTERN)
+    ]),
     life: new FormControl(this.pokemon().life),
     damage: new FormControl(this.pokemon().damage),
     types: new FormArray(
